9268d9555864851702b5d0664bf83e8e4c24186e
Faizan <qfaizan489@gmail.com>
2022-11-30 20:16:07 +0500

    cyclic cloud

--- server.mjs
+++ server.mjs
@@ -2,14 +2,17 @@ console.log('hello world as module javascript')
 
 import express   from "express"
 const app = express()
-const port = 3000
+const port = process.env.Port || 3000
 
 app.get('/', (req, res) => {
     console.log('request ip:',req.ip)
-  res.send('Hello World!' + new Date().toString())
+  res.send('Hello Faizan Qureshi!' + new Date().toString())
 })
 //Ip Address From Network properties
 //http://192.168.43.166:3000
+
+//cloud link
+//https://good-lime-buffalo-gear.cyclic.app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
