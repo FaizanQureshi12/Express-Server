ff69f9230c8c6998c102c938ef59a8474cac0a44
Faizan <qfaizan489@gmail.com>
2022-12-02 20:37:14 +0500

    react

--- server.mjs
+++ server.mjs
@@ -1,13 +1,18 @@
-console.log('hello world as module javascript')
+// console.log('hello world as module javascript')
 
 import express   from "express"
+import path from 'path'
 const app = express()
 const port = process.env.Port || 3000
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
     console.log('request ip:',req.ip)
   res.send('Hello Faizan Qureshi!' + new Date().toString())
 })
+ 
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './web/build')))
+
 //Ip Address From Network properties
 //http://192.168.43.166:3000
 
