85ccb435e32a5a03a1cbc2801f4c259e8826fb71
Faizan <qfaizan489@gmail.com>
2022-12-03 19:15:14 +0500

    data

--- server.mjs
+++ server.mjs
@@ -1,17 +1,32 @@
-console.log('hello world as module javascript')
-
 import express   from "express"
 import path from 'path'
-const app = express()
-const port = process.env.Port || 3000
 
+const app = express()
+const port = process.env.Port || 5001
+console.log('hello world as module javascript')
+ 
 app.get('/abc', (req, res) => {
     console.log('request ip:',req.ip)
   res.send('Hello Faizan Qureshi!' + new Date().toString())
 })
+
+app.get('/weather', (req, res) => {
+  console.log('request ip:',req.ip)
+res.send(
+  { 
+    temp:20,
+    humidity:63,
+    serverTime: new Date().toString()
+  });
+})
+app.get('/gettime', (req, res) => {
+  console.log('request ip:',req.ip)
+res.send('Hello Faizan Qureshi!' + new Date().toString())
+})
  
 const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
 
 //Ip Address From Network properties
 //http://192.168.43.166:3000
