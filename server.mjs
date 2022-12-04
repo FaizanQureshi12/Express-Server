2c6b8cd2fd64b77bc3656a764061e93f2d7fdb74
Faizan <qfaizan489@gmail.com>
2022-12-04 19:27:49 +0500

    ok

--- server.mjs
+++ server.mjs
@@ -1,33 +1,42 @@
-import express   from "express"
+import express from "express"
 import path from 'path'
 import cors from 'cors'
 
 const app = express()
 const port = process.env.Port || 5001
 console.log('hello world as module javascript')
-   
+
+app.use(express.json());
 app.use(cors());
 app.get('/abc', (req, res) => {
-    console.log('request ip:',req.ip)
+  console.log('request ip:', req.ip)
   res.send('Hello Faizan Qureshi!' + new Date().toString())
 })
 
-app.get('/weather', (req, res) => {
-  console.log('request ip:',req.ip)
-res.send(
-  { 
-    temp:20,
-    min:10,
-    max:50,
-    humidity:63,
-    serverTime: new Date().toString()
-  });
+//url params
+//http://localhost:5001/weather/karachi
+//app.get('/weather/:cityName', (req, res) => {
+
+//query parameters
+//http://localhost:5001/weather?city=karachi& 
+app.get('/weather/:cityName', (req, res) => {
+  console.log('request ip:', req.ip)
+  console.log('param:', req.params.cityName)
+  res.send(
+    {
+      city: req.params.cityName,
+      temp: 20,
+      min: 10,
+      max: 50,
+      humidity: 63,
+      serverTime: new Date().toString()
+    });
 })
 app.get('/gettime', (req, res) => {
-  console.log('request ip:',req.ip)
-res.send('Hello Faizan Qureshi!' + new Date().toString())
+  console.log('request ip:', req.ip)
+  res.send('Hello Faizan Qureshi!' + new Date().toString())
 })
- 
+
 const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname, './web/build')))
 app.use('*', express.static(path.join(__dirname, './web/build')))
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,6 +3,11 @@ import './App.css';
 import { useState } from "react"
 import axios from "axios";
 
+let baseUrl = ``
+if (window.location.href.split(':')[0] === 'http') {
+    baseUrl = `http://localhost:5001`
+}
+
 function App() {
 
     const [weatherData, setWeatherData] = useState(null)
@@ -10,12 +15,10 @@ function App() {
 
     const submitHandler = (e) => {
         e.preventDefault();
-
         console.log("I am click handler")
-        axios.get(`http://localhost:5001/weather`)
+        axios.get(`${baseUrl}/weather/${cityName}`)
             .then(response => {
                 console.log("response: ", response.data);
-
                 setWeatherData(response.data);
             })
             .catch(err => {
@@ -24,21 +27,20 @@ function App() {
     }
 
     return (
-        <div>
-
-            <form onSubmit={submitHandler}>
+        <div >
+            <form onSubmit={submitHandler} id='get'>
                 City Name:
-                <input type="text" placeholder="enter your city name" required
-                    onChange={(e) => { setCityName(e.target.value) }} />
-
-                <button type="submit">get weather</button>
+                <input type="text" id='put' placeholder="Enter your city name" 
+                required onChange={(e) => { setCityName(e.target.value) }} /> 
+                <button type="submit" id='bttn'>Get weather</button>
             </form>
             <br />
             <br />
 
             {(weatherData === null) ? null :
-                <div>
-
+                <div id='result'>
+                    City: {weatherData?.city}
+                    <br />
                     Temperature: {Math.round(weatherData?.temp)}°C
                     <br />
                     min: {Math.round(weatherData?.min)}°C
